c4fd501374d8deca4a086d7ee821f4ff6dbf6cbe
Jovany S. Opelina <102904687+bankaihekai@users.noreply.github.com>
2023-10-04 00:09:02 +0800

    Update ProductDetailPage.js

--- Route Guide/Route Basic/pages/ProductDetailPage.js
+++ Route Guide/Route Basic/pages/ProductDetailPage.js
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+// absolute path with '/' while relative does not 
+import { useParams, Link } from "react-router-dom";
 
 function ProductDetailPage() {
   const params = useParams();
@@ -7,6 +8,8 @@ function ProductDetailPage() {
     <>
       <h1>Product Details</h1>
       <p>{params.productId}</p>
+      
+      // <Link to='..' relative='path' >Go Back</Link>
     </>
   );
 }
